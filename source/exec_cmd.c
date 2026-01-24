4f5a10bd0022d100559e78cfb6b667804758e2bb
Damian Parrino <bucanero@users.noreply.github.com>
2026-01-24 16:07:09 -0300

    Update exec_cmd.c

--- source/exec_cmd.c
+++ source/exec_cmd.c
@@ -1220,7 +1220,7 @@ static int apply_cheat_patches(const save_entry_t* entry)
 			}
 		}
 
-		if (!apply_cheat_patch_code(tmpfile, entry->title_id, code, &vita_host_callback))
+		if (!apply_cheat_patch_code(tmpfile, code, &vita_host_callback))
 		{
 			LOG("Error: failed to apply (%s)", code->name);
 			ret = 0;
